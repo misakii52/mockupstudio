f3f612a15c28404eb67137573d921cbb02a395b3
misakii52 <ugurbayiroglu52@gmail.com>
2026-02-05 15:44:57 +0300

    Update firebase.js

--- assets/js/firebase.js
+++ assets/js/firebase.js
@@ -1,1 +1,15 @@
+// FIREBASE CONFIG
+const firebaseConfig = {
+  apiKey: "AIzaSyDs6JHPSV8UM2uKSSVUA1SNxWGyxAihaII",
+  authDomain: "mockup-master-db.firebaseapp.com",
+  databaseURL: "https://mockup-master-db-default-rtdb.firebaseio.com",
+  projectId: "mockup-master-db",
+  storageBucket: "mockup-master-db.appspot.com",
+  messagingSenderId: "68063883147",
+  appId: "1:68063883147:web:07debadc7efe4aeccac98e"
+};
+
+// INIT
+firebase.initializeApp(firebaseConfig);
+const db = firebase.database();
 
