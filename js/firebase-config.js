5e7466e50f0d34e12bb978126d1586572d039876
misakii52 <ugurbayiroglu52@gmail.com>
2026-02-06 11:23:08 +0300

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,1 +1,20 @@
+// Firebase Configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDs6JHPSV8UM2uKSSVUA1SNxWGyxAihaII",
+    authDomain: "mockup-master-db.firebaseapp.com",
+    projectId: "mockup-master-db",
+    storageBucket: "mockup-master-db.firebasestorage.app",
+    messagingSenderId: "68063883147",
+    appId: "1:68063883147:web:07debadc7efe4aeccac98e"
+};
 
+// Initialize Firebase
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-firestore.js";
+import { getAuth, signInWithEmailAndPassword, signOut, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-auth.js";
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const auth = getAuth(app);
+
+export { db, auth, signInWithEmailAndPassword, signOut, onAuthStateChanged };
