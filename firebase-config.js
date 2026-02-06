21c81f01b0c3c99d8e6cabe932c02136187779d8
misakii52 <ugurbayiroglu52@gmail.com>
2026-02-06 10:56:12 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,1 +1,11 @@
-
+const firebaseConfig = {
+    apiKey: "AIzaSyDs6JHPSV8UM2uKSSVUA1SNxWGyxAihaII",
+    authDomain: "mockup-master-db.firebaseapp.com",
+    databaseURL: "https://mockup-master-db-default-rtdb.firebaseio.com",
+    projectId: "mockup-master-db",
+    storageBucket: "mockup-master-db.firebasestorage.app",
+    messagingSenderId: "68063883147",
+    appId: "1:68063883147:web:07debadc7efe4aeccac98e"
+};
+firebase.initializeApp(firebaseConfig);
+const db = firebase.database();
